ea5d1538f9aefd21592cccc6c2cdd9979e1d241c
EmmaJr11 <corliuslilkhleb@gmail.com>
2023-06-22 20:03:06 -0700

    Write a function that prints the numbers, from 0 to 9, followed by a new line

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - function that checks for uppercase character.
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+
+	return(0);
+}
